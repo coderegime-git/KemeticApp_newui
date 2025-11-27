968df03050c423aa9d72d3909092a78d74f07a7b
NagarajanIXM <snraj1991@gmail.com>
2025-11-27 12:12:28 +0530

    reels change

--- routes/api/reels.php
+++ routes/api/reels.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\Api\ReelController;
 use App\Http\Controllers\Api\AdminReelController;
 
+ Route::prefix('reels')->group(function () {
+        Route::get('/', [ReelController::class, 'index']);
+     });
+
 Route::middleware(['auth:api'])->group(function () {
     // Public Reel Routes
     Route::prefix('reels')->group(function () {
-        Route::get('/', [ReelController::class, 'index']);
+        //Route::get('/', [ReelController::class, 'index']);
         Route::post('/', [ReelController::class, 'store']);
         Route::get('/reel/{reel}', [ReelController::class, 'show']);
         Route::delete('/{reel}', [ReelController::class, 'destroy']);
