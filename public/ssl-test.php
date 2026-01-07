880f86902e3701af84aa764fbff12cc3cac92f12
NagarajanIXM <snraj1991@gmail.com>
2026-01-07 18:50:34 +0530

    livestream and notification changes

--- public/ssl-test.php
+++ public/ssl-test.php
@@ -1,10 +1,11 @@
 <?php
-$ch = curl_init("https://oauth2.googleapis.com/token");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_exec($ch);
+    $ch = curl_init("https://oauth2.googleapis.com/token");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
 
-if (curl_errno($ch)) {
-    echo "ERROR: " . curl_error($ch);
-} else {
-    echo "SSL WORKING ✅";
-}
+    if (curl_errno($ch)) {
+        echo "ERROR: " . curl_error($ch);
+    } else {
+        echo "SSL WORKING ✅";
+    }
+?>
